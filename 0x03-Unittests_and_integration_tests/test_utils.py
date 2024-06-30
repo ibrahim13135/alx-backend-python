93d94ff8c62966d93b57407eedb2598860d7fd10
Hima <abrahimmahmoud5454@gmail.com>
2024-06-30 21:14:59 +0300

    first task TestAccessNestedMap

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,27 +1,45 @@
-import unittest
+#!/usr/bin/env python3
 
+import unittest
 
 from typing import Dict, Tuple, Union
 
-
+# run the test method with different sets of parameters.
 from parameterized import parameterized
 
-
+# Access nested map with key path.
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function."""
+    """
+    TestAccessNestedMap class to test access_nested_map function.
+    """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
-        """Tests `access_nested_map`'s output."""
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str], expected: Union[Dict, int],) -> None:
+        """
+        Test that access_nested_map returns the expected result.
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+# nested_map={"a": 1}, path=("a",)
+# nested_map={"a": {"b": 2}}, path=("a",)
+# nested_map={"a": {"b": 2}}, path=("a", "b")
+
+# @parameterized.expand([
+#     (input1, input2, expected_output),
+#     (input3, input4, expected_output),
+#     ...
+# ])
+
+
+# @parameterized.expand([
+#     ({"a": 1}, ("a",), 1),
+#     ({"a": {"b": 2}}, ("a",), {"b": 2}),
+#     ({"a": {"b": 2}}, ("a", "b"), 2)
+# ])
