49edcad72f2ef3c7b5e03d3c7b008ff1aba1f76f
Hima <abrahimmahmoud5454@gmail.com>
2024-06-30 21:13:28 +0300

    first task TestAccessNestedMap

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,22 +11,17 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    TestAccessNestedMap class to test access_nested_map function.
-    """
-
+    """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self,
-                               nested_map: Dict,
-                               path: Tuple[str],
-                               expected: Union[Dict,
-                                               int],
-                               ) -> None:
-        """
-        Test that access_nested_map returns the expected result.
-        """
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
