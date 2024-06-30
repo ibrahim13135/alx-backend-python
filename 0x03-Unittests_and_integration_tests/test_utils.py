8abe6b569ffa79bda723b0a7de97428e3778d113
Hima <abrahimmahmoud5454@gmail.com>
2024-07-01 01:17:39 +0300

    task 4 first memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -81,7 +81,9 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
     def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
         class TestClass:
             def a_method(self):
                 return 42
@@ -89,23 +91,12 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-
-        # Create an instance of TestClass
-
-        # Patch a_method to track its calls
-        with patch.object(TestClass, 'a_method', return_value=42) as moc_mt:
-            # Call a_property twice
-            test_obj = TestClass()
-            result1 = test_obj.a_property
-            result2 = test_obj.a_property
-
-            # Assert that the correct result is returned
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
-
-            # Assert that a_method was only called once
-            moc_mt.assert_called_once()
-
-
-if __name__ == '__main__':
-    unittest.main()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
