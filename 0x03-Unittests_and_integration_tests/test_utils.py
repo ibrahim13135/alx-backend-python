1caa8a178ffdf8207904a68cdb16fea6b28f9ede
Hima <abrahimmahmoud5454@gmail.com>
2024-06-30 21:10:30 +0300

    first task TestAccessNestedMap

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -30,20 +30,3 @@ class TestAccessNestedMap(unittest.TestCase):
         Test that access_nested_map returns the expected result.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-# nested_map={"a": 1}, path=("a",)
-# nested_map={"a": {"b": 2}}, path=("a",)
-# nested_map={"a": {"b": 2}}, path=("a", "b")
-
-# @parameterized.expand([
-#     (input1, input2, expected_output),
-#     (input3, input4, expected_output),
-#     ...
-# ])
-
-
-# @parameterized.expand([
-#     ({"a": 1}, ("a",), 1),
-#     ({"a": {"b": 2}}, ("a",), {"b": 2}),
-#     ({"a": {"b": 2}}, ("a", "b"), 2)
-# ])
