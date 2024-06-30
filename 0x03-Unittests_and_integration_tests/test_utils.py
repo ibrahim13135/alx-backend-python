24df21075657d20878d6ec77d43fdf8b5a78c93e
Hima <abrahimmahmoud5454@gmail.com>
2024-07-01 01:16:54 +0300

    task 4 first memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -81,7 +81,7 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    def test_memoize(self):
+    def test_memoize(self) -> None:
         class TestClass:
             def a_method(self):
                 return 42
